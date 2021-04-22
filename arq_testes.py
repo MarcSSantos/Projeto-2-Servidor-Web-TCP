6c4222e42b0ab3fa76c97195b7e606f9c0135355
aslayclevisson <aslayclei@gmail.com>
2021-04-22 13:26:24 -0300

    pequenas modificações e alocações

--- arq_testes.py
+++ arq_testes.py
@@ -21,3 +21,59 @@ print(pasta_ou_arquivo)
 modificacao = os.path.getmtime(g)
 modificacao_local = time.ctime(modificacao)
 print(modificacao_local)
+
+i = [1,2,3,4]
+i.pop(len(i)-1)
+print(i)
+
+def kk():
+  lista = [0,1,2,3,4]
+  return lista
+
+j = kk()[0:2]
+print(j, 'bumbum')
+
+
+u = ['']
+print(u[-1],'aaaa')
+
+def pop_dir(diretorio):
+    # C:\Projeto_servidor_web\arq\omega
+    restringir = diretorio.split('\\')
+    if restringir[-1] != 'arq':
+        restringir.pop(len(restringir)-1)
+        
+        if restringir[-1] == 'arq':
+            return ['']
+        else:
+            return restringir
+    
+    else:
+        return  ['']
+      
+sim = ''      
+ll = f'{"g"}'
+print(ll)
+
+#                if arquivo_requisitado == '\\abacate':
+#                    lista_back_dir = pop_dir(diretorio_atual)
+#                    diretorio_atual = join_method(lista_back_dir)
+
+for x in range(0, 0):
+  print('pipoca')
+  
+t  = [8,7,6,4]
+
+yy = t.index(8)
+print(yy) 
+
+def join_method(lista):
+  return ''.join(['\\'+x if x != lista[0] else x for x in lista])
+
+
+ii = ['a','b','c','d']
+uu = join_method(ii)
+print(uu)
+
+for x in range(2,3):
+  print(x)
